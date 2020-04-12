7299005a0bf94c861c18e23c157eecf1590f1522
Angelo Sepulveda <angelo.sv@outshifter.com>
2020-04-12 16:14:09 +0200

    writing in the DB

--- functions/api/routes/orders.js
+++ functions/api/routes/orders.js
@@ -19,16 +19,24 @@ router.get('/', checkAuth, (req, res, next)=> {
 });
 
 router.post('/new', (req, res)=>{
-    console.log(req.body.order_id)
+    console.log(req.body)
      var order_id = req.body.order_id
+
+     var order_items = req.body.data_order.items;
+    var order_customer = req.body.data_order.customer;
+    var order_billing_address = req.body.data_order.billing_address;
+    var order_shipping = req.body.data_order.shipping;
+   
 let order =  db.collection('orders').doc(order_id).set({
-    'first': 'Alan',
-    'middle': 'Mathison',
-    'last': 'Turing',
-    'born': 1912
+        order_items: order_items,
+        order_customer: order_customer,
+        order_billing_address: order_billing_address,
+
   });
+  console.log(res)
     res.status(200).json({
-        message: 'order was placed succesfull'
+        message: 'order was placed succesfull',
+        
     })
 })
 
