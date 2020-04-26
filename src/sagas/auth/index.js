908960f4d7b6cf6eb023cb72f1dca721d0a4f5ae
Angelo Sepulveda <angelo.sv@outshifter.com>
2020-04-26 12:50:48 +0200

    login working

--- src/sagas/auth/index.js
+++ src/sagas/auth/index.js
@@ -1,4 +1,7 @@
+import {auth} from '../../firebase';
+
 import {all, put, takeEvery, select} from  'redux-saga/effects';
+
 import {
     LOGIN_USER_FETCH,
     LOGIN_USER_FETCH_SUCCESS,
@@ -14,7 +17,7 @@ import {
     FIREBASE_CHANGE_EMAIL,
     FIREBASE_REGISTER_USER_INIT,
 } from '../../constants/ActionTypes'
-
+import { firebaseLoginSucces, firebaseLoginError } from '../../actions';
 
 function* startRegister(){
     console.log('Desde saga')
@@ -23,8 +26,16 @@ function* startRegister(){
 
 
 function* startLogin({payload}){
-    console.log(payload)
-    console.log('hola desde startLogin')
+    const {email, password } = payload;
+    try {
+        const authUser = yield auth.signInWithEmailAndPassword(email, password);
+        const userId = authUser.user.uid;
+        yield put(firebaseLoginSucces(userId));
+        localStorage.setItem('authUser',userId)
+    } catch (error) {
+        console.log(error.message)
+        yield put(firebaseLoginError(error.message))
+    }
     
 }
 
